430a8381a7f0a6277ef5a0bda594817f09574c8f
gayeon0417 <142369974+gayeon0417@users.noreply.github.com>
2023-09-26 13:17:01 +0900

    Add files via upload

--- js/tab.js
+++ js/tab.js
@@ -2,10 +2,22 @@ $(function () {
     /*.more_btn a를 클릭하면, .popup이 나타난다.
     .close를 클릭하면 .popup이 사라진다.
     */
-    $(".ongoing li").click(function () {
+    $(".ongoing .pop1").click(function () {
         $(".popup").fadeIn();
     });
     $(".close_btn").click(function () {
         $(".popup").fadeOut();
+    });
+    $(".ongoing .pop2").click(function () {
+        $(".popup2").fadeIn();
+    });
+    $(".close_btn").click(function () {
+        $(".popup2").fadeOut();
+    });
+    $(".ongoing .pop3").click(function () {
+        $(".popup3").fadeIn();
+    });
+    $(".close_btn").click(function () {
+        $(".popup3").fadeOut();
     });
 });
